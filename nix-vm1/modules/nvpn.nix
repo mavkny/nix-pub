bd6abcecd4bf4e0abb62ec1f712a42b54cd0098e
mavkny <101067185+mavkny@users.noreply.github.com>
2025-09-09 20:11:38 +0200

    Update nvpn.nix

--- nix-vm1/modules/nvpn.nix
+++ nix-vm1/modules/nvpn.nix
@@ -1,16 +1,12 @@
 { config, pkgs, ... }:
 
 {
-  # OpenVPN-Service für NordVPN
   services.openvpn.servers.nordvpn = {
-    # Lies deine heruntergeladene .ovpn Datei ein
-    config = builtins.readFile "/etc/openvpn/nordvpn.ovpn";
+    autoStart = true;
+    updateResolvConf = true;
 
-    autoStart = true;         # startet beim Booten
-    updateResolvConf = true;  # setzt DNS automatisch
-
-    extraConfig = ''
-      # Hier wird auf deine Credentials-Datei verwiesen
+    config = ''
+      config /etc/openvpn/nordvpn.ovpn
       auth-user-pass /etc/openvpn/nordvpn-auth.txt
       persist-key
       persist-tun
@@ -18,11 +14,9 @@
     '';
   };
 
-  # DNS über systemd-resolved
   services.resolved.enable = true;
   networking.networkmanager.enable = true;
   networking.networkmanager.dns = "systemd-resolved";
 
-  # Firewall kann so bleiben; UDP-Port 1194 ist Standard für NordVPN
   networking.firewall.allowedUDPPorts = [ 1194 ];
 }
