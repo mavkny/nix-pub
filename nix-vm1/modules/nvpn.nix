c814d8a434794c4a1f8c845668d3207b34410391
mavkny <101067185+mavkny@users.noreply.github.com>
2025-09-09 20:16:38 +0200

    Update nvpn.nix

--- nix-vm1/modules/nvpn.nix
+++ nix-vm1/modules/nvpn.nix
@@ -3,7 +3,6 @@
 {
   services.openvpn.servers.nordvpn = {
     autoStart = true;
-    updateResolvConf = true;
 
     config = ''
       config /etc/openvpn/nordvpn.ovpn
@@ -11,12 +10,22 @@
       persist-key
       persist-tun
       verb 3
+
+      # Wichtig: DNS explizit setzen
+      script-security 2
+      up /etc/openvpn/update-systemd-resolved
+      down /etc/openvpn/update-systemd-resolved
+      down-pre
     '';
   };
 
+  # DNS über systemd-resolved
   services.resolved.enable = true;
+
+  # sicherstellen, dass systemd-resolved für alles genutzt wird
   networking.networkmanager.enable = true;
   networking.networkmanager.dns = "systemd-resolved";
 
+  # Firewall
   networking.firewall.allowedUDPPorts = [ 1194 ];
 }
